45acf431d59d4e69d0e801ab71bec4cfeaed6a9b
bukwon <dnu05043@naver.com>
2024-04-14 16:38:48 +0900

    step08

---
+++ src/inflearn/step08.java
@@ -0,0 +1,19 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class step08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(palindrome(str));
+    }
+
+    private static String palindrome(String str) {
+        String answer = "NO";
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+        String tmp = new StringBuilder(str).reverse().toString();
+        if (str.equals(tmp)) answer = "YES";
+        return answer;
+    }
+}
