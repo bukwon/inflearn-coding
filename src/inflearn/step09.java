ca63f7202fcb13f63d52e7845032e6e2efd4bc97
bukwon <dnu05043@naver.com>
2024-04-15 17:02:36 +0900

    step09

---
+++ src/inflearn/step09.java
@@ -0,0 +1,19 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class step09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(onlyNumber(str));
+    }
+
+    private static int onlyNumber(String str) {
+        String answer = "";
+        for (char x: str.toCharArray()) {
+            if (Character.isDigit(x)) answer += x;//answer = answer * 10 + (x-48);
+        }
+        return Integer.parseInt(answer);
+    }
+}
