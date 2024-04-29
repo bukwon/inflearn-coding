4281742285cdd76235d70dc4f6f3478ddab5a85c
bukwon <dnu05043@naver.com>
2024-04-29 16:35:09 +0900

    reTest-section2-step04re

---
+++ src/inflearn/etc/baekjoon/baekjoon/no17413.java
@@ -0,0 +1,42 @@
+package inflearn.etc.baekjoon.baekjoon;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class no17413 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(solution(s));
+    }
+
+    private static String solution(String s) {
+        String answer = "";
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        boolean check1 = false;
+        s += ' ';
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '<') {
+                while(!stack.isEmpty()) sb.append(stack.pop());
+                sb.append(s.charAt(i));
+                check1 = true;
+            } else if(s.charAt(i) == '>') {
+                sb.append(s.charAt(i));
+                check1 = false;
+            }
+            else if(check1) {
+                sb.append(s.charAt(i));
+            } else{
+                if(s.charAt(i) == ' ' || i == s.length()-1) {
+                    while(!stack.isEmpty()) sb.append(stack.pop());
+                    if(s.charAt(i) == ' ') sb.append(' ');
+                    continue;
+                }
+                stack.add(s.charAt(i));
+            }
+        }
+        return String.valueOf(sb);
+    }
+}
--- src/inflearn/inflearn/inflearn/test/test.java
+++ src/inflearn/inflearn/inflearn/test/test.java
@@ -1,11 +1,57 @@
 package inflearn.inflearn.inflearn.test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Stack;
 
 public class test {
-    public static void main(String[] args) {
-        HashMap<String, Integer> arr = new HashMap<>();
-        int count = 1;
-        arr.put("txt", count);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String s = br.readLine();
+        Stack<Character> stack = new Stack<>();
+
+        boolean check = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '<'){
+                check = true;
+                while(!stack.empty()){
+                    sb.append(stack.pop());
+                }
+                sb.append(s.charAt(i));
+            }
+
+            else if(s.charAt(i) == '>'){
+                check = false;
+                sb.append(s.charAt(i));
+            }
+
+            else if(check){
+                sb.append(s.charAt(i));
+            }
+
+            else if(!check){
+                if(s.charAt(i) == ' '){
+                    while(!stack.empty()){
+                        sb.append(stack.pop());
+                    }
+                    sb.append(s.charAt(i));
+                }
+
+                else {
+                    stack.push(s.charAt(i));
+                }
+            }
+        }
+
+        while(!stack.empty()){
+            sb.append(stack.pop());
+        }
+
+        System.out.println(sb.toString());
     }
+
 }
