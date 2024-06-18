ab5b8ae02b1c658d8240a2ad0926d8ed72680eb7
BuKwon Kim <dnu05043@naver.com>
2024-06-19 00:05:58 +0900

    leetcode-no1529

--- src/inflearn/myAlgorithmn/leetcode/no1529.java
+++ src/inflearn/myAlgorithmn/leetcode/no1529.java
@@ -6,41 +6,57 @@ public class no1529 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String target = sc.next();
-        System.out.println(solution(target));
+        System.out.print(solution(target));
     }
 
     private static int solution(String target) {
         int answer = 0;
-        char[] tmp = new char[target.length()];
-        String check = "";  // while 문에서 target과 같은지 확인하는 용도
+        char check = '0';
         for (int i = 0; i < target.length(); i++) {
-            tmp[i] += '0';
-            check += "0";
-        }
-        while(!check.equals(target)) {
-            boolean flag = false; // 끝 값이 서로 다른 경우
-            if (check.charAt(target.length() - 1) == target.charAt(target.length() - 1)) flag = true;
-            for (int i = target.length() - 1; i >= 0; i--) {
-                if (flag) {
-                    if (check.charAt(i) != target.charAt(i)) flag = false;
-
-                    if (check.charAt(i) == '0') tmp[i] = '1';
-                    else tmp[i] = '0';
-
-                    if (!flag) break;
-                } else {
-                    if (check.charAt(i) == '0') tmp[i] = '1';
-                    else tmp[i] = '0';
-
-                    if (i > 0 && check.charAt(i - 1) == target.charAt(i - 1)) break;
-                }
+            if (target.charAt(i) != check) {
+                answer ++;
+                check = target.charAt(i);
             }
-            check = "";
-            for (char c: tmp) {
-                check += c;
-            }
-            answer ++;
         }
         return answer;
     }
+
+
+
+    /*private static int solution(String target) {
+        int answer = 0;
+        int countNum = 0;   // 연속적인 1의 수를 세기 위함
+        boolean flag = true; // 1과 0 교차 바꾸기 위한 변수 (false일 때 0, true면 1)
+        char[] tmp = new char[target.length()];
+        String check = "";  // while 문에서 target과 같은지 확인하는 용도
+
+        for (int i = 0; i < target.length(); i++) {
+            tmp[i] = '0';
+            check += "0";
+            if (target.charAt(target.length() - i - 1) == '1') countNum = target.length() - i - 1;
+        }
+
+        int j = 0;  // 0과 1의 자리 추적
+        while (!target.equals(check)) {
+            check = "";
+            for (int i = target.length() - 1; i >= countNum; i--) {
+                if (flag) {
+                    tmp[i] = '1';
+                    if (target.charAt(i) == '0') j = i;
+                }
+                else {
+                    tmp[i] = '0';
+                    if (target.charAt(i) == '1') j = i;
+                }
+            }
+
+            for (char c: tmp) check += c;
+
+            flag = !flag;
+            countNum = j;
+            answer ++;
+        }
+        return answer;
+    }*/
 }
+
